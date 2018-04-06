596dde5078f0dbeacb6a1d713a0b2ab18a729efb
kunlayako <kunlayajwry@hotmail.com>
2018-04-06 11:00:12 -0700

    Initial Commit

---
+++ challengeApp1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  challengeApp1
+//
+//  Created by Kunlaya Kobunnoi on 2018-04-06.
+//  Copyright © 2018 Kunlaya Kobunnoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
