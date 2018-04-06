4353a9ac1a479cb173524bf075a10e2927a4623a
kunlayako <kunlayajwry@hotmail.com>
2018-04-06 14:36:52 -0700

    head

--- challengeApp1/ViewController.swift
+++ challengeApp1/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        compassAvail()
         // Do any additional setup after loading the view, typically from a nib.
     }
     @IBOutlet weak var Compass: UITextField!
